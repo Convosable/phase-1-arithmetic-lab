d09dc8488c05e16e31f952a525f028f81bfd70b7
Connor Vosberg <connorjv@gmail.com>
2022-11-02 19:21:50 -0700

    commit tree d535099f226cf5944616101303f70e5a5de7516c

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 15.5
+let num2 = 4
+let multiply = num1*num2
+console.log(multiply)
 
-// Write your code here
+let random = Math.floor(Math.random()*10)
+console.log(random)
+
+let num3 = 24
+let num4 = 10
+let mod = num3%num4
+console.log(mod)
+
+const max = Math.max(1,2,5,3,7,9,20)
+console.log(max)
